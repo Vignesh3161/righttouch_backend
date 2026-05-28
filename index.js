ee166af44feb0d4dd24a41a6e45e99209792aacb
vishnu <vishnuak@codiis.com>
2026-05-28 22:48:07 +0530

    change port

--- index.js
+++ index.js
@@ -344,7 +344,7 @@ App.use((err, req, res, next) => {
 
 const port = parseInt(process.env.PORT, 10) || 8080;
 
-httpServer.listen(port, () => {
+httpServer.listen(port, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${port}`);
   console.log(`🔌 Socket.IO ready for real-time notifications`);
 });
