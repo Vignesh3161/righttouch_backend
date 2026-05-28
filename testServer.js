fbf6c3ee0772ee3cb50bda4d38e0b14677e2092f
vishnu <vishnuak@codiis.com>
2026-05-28 23:04:45 +0530

    mongo test

--- testServer.js
+++ testServer.js
@@ -1,9 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("Mongo Connected"))
+.catch(err => console.log(err));
+
 app.get("/", (req, res) => {
-  res.send("Cloud Run Working");
+  res.send("Mongo Working");
 });
 
 const port = process.env.PORT || 8080;
